6b0344b0698e867d0224702f206cc6c0b497bfdb
Chris <christmarch@outlook.com>
2025-03-05 19:11:56 +1000

    style: unify font sizes for better consistency on login and registration pages

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -86,13 +86,13 @@ export default function Register() {
                 <div class="min-h-screen flex flex-col items-center justify-center py-6 px-4 mt-5">
                     <div class="max-w-md w-full">
                         <div class="p-8 rounded-2xl bg-white bg-opacity-5">
-                            <h2 class="text-white text-center 2xl:text-2xl xl:text-lg font-bold">Create Account</h2>
+                            <h2 class="text-white text-center 2xl:text-2xl text-lg font-bold">Create Account</h2>
                             <form class="mt-8 space-y-4" onSubmit={handleSubmit}>
-                                {error && <p class="text-red-500 2xl:text-sm xl:text-xs text-center">{error}</p>}
+                                {error && <p class="text-red-500 2xl:text-sm text-xs text-center">{error}</p>}
                                 <div>
-                                    <label class="text-white 2xl:text-sm xl:text-xs mb-2 block">Email</label>
+                                    <label class="text-white 2xl:text-sm text-xs mb-2 block">Email</label>
                                     <div class="relative flex items-center">
-                                        <input maxLength="200" name="email" type="text" required class="w-full bg-white bg-opacity-5 text-neutral-300 2xl:text-sm xl:text-xs border border-white border-opacity-15 px-4 py-3 rounded-md focus:outline-none" placeholder="Enter Email" />
+                                        <input maxLength="200" name="email" type="text" required class="w-full bg-white bg-opacity-5 text-neutral-300 2xl:text-sm text-xs border border-white border-opacity-15 px-4 py-3 rounded-md focus:outline-none" placeholder="Enter Email" />
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4" viewBox="0 0 24 24">
                                             <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
                                             <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
@@ -101,25 +101,25 @@ export default function Register() {
                                 </div>
 
                                 <div>
-                                    <label class="text-white 2xl:text-sm xl:text-xs mb-2 block">Password</label>
+                                    <label class="text-white 2xl:text-sm text-xs mb-2 block">Password</label>
                                     <div class="relative flex items-center">
-                                        <input maxLength="200" name="password" type="password" required class="w-full bg-white bg-opacity-5 text-neutral-300 2xl:text-sm xl:text-xs border border-white border-opacity-15 px-4 py-3 rounded-md focus:outline-none" placeholder="Enter password" />
+                                        <input maxLength="200" name="password" type="password" required class="w-full bg-white bg-opacity-5 text-neutral-300 2xl:text-sm text-xs border border-white border-opacity-15 px-4 py-3 rounded-md focus:outline-none" placeholder="Enter password" />
                                     </div>
                                 </div>
 
                                 <div>
-                                    <label class="text-white 2xl:text-sm xl:text-xs mb-2 block">Confirm Password</label>
+                                    <label class="text-white 2xl:text-sm text-xs mb-2 block">Confirm Password</label>
                                     <div class="relative flex items-center">
-                                        <input maxLength="200" name="confirmPassword" type="password" required class="w-full bg-white bg-opacity-5 text-neutral-300 2xl:text-sm xl:text-xs border border-white border-opacity-15 px-4 py-3 rounded-md focus:outline-none" placeholder="Enter password" />
+                                        <input maxLength="200" name="confirmPassword" type="password" required class="w-full bg-white bg-opacity-5 text-neutral-300 2xl:text-sm text-xs border border-white border-opacity-15 px-4 py-3 rounded-md focus:outline-none" placeholder="Enter password" />
                                     </div>
                                 </div>
 
                                 <div class="!mt-8">
-                                    <button type="submit" class="w-full py-3 px-4 2xl:text-sm xl:text-xs tracking-wide rounded-lg text-white bg-purple-400 hover:bg-purple-500 focus:outline-none transition duration-300">
+                                    <button type="submit" class="w-full py-3 px-4 2xl:text-sm text-xs tracking-wide rounded-lg text-white bg-purple-400 hover:bg-purple-500 focus:outline-none transition duration-300">
                                         Create an account
                                     </button>
                                 </div>
-                                <p class="text-white 2xl:text-sm xl:text-xs !mt-8 text-center">Already have an account? <a href="/login" class="text-purple-300 hover:underline ml-1 whitespace-nowrap font-semibold ">Login here</a></p>
+                                <p class="text-white 2xl:text-sm text-xs !mt-8 text-center">Already have an account? <a href="/login" class="text-purple-300 hover:underline ml-1 whitespace-nowrap font-semibold ">Login here</a></p>
                             </form>
                         </div>
                     </div>
